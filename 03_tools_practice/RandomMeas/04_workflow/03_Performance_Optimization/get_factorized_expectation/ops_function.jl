c16daaaeb28d5c92dabfd676b2c65c2a732ef19d
git_usr <a18762608798@petalmail.com>
2026-05-17 15:07:53 +0800

    format

--- 03_tools_practice/RandomMeas/04_workflow/03_Performance_Optimization/get_factorized_expectation/ops_function.jl
+++ 03_tools_practice/RandomMeas/04_workflow/03_Performance_Optimization/get_factorized_expectation/ops_function.jl
@@ -1,12 +1,14 @@
 using RandomMeas
 
 # create local swap op
-function create_local_swap_op(bitsnum::Int64, localsites::Vector{Int64}, siteindices::Vector{Index{Int64}})
+function create_local_swap_op(
+    bitsnum::Int64, localsites::Vector{Int64}, siteindices::Vector{Index{Int64}}
+)
     ops = Vector{MPO}(undef, 4)
     site1 = localsites[1]
     site2 = localsites[2]
     opstrbases = ["I", "X", "Y", "Z"]
-    for basesindex = 1:4
+    for basesindex in 1:4
         opstrbase = opstrbases[basesindex]
         opstrs = ["I" for _ in 1:bitsnum]
         opstrs[site1], opstrs[site2] = opstrbase, opstrbase
@@ -22,7 +24,7 @@ function create_reflect_op(bitsnum::Int64, siteindices::Vector{Index{Int64}})
     pairsnum = bitsnum ÷ 2
     opstrs = ["I" for _ in 1:bitsnum]
     op = MPO(ComplexF64, siteindices, opstrs)
-    for pairsite = 1:pairsnum
+    for pairsite in 1:pairsnum
         site1 = pairsite
         site2 = bitsnum - site1 + 1
         sites = [site1, site2]
@@ -38,28 +40,28 @@ function create_adjacent_swap_op(bitsnum::Int64, siteindices::Vector{Index{Int64
     pairsnum = bitsnum ÷ 2
     opstrs = ["I" for _ in 1:bitsnum]
     op = MPO(ComplexF64, siteindices, opstrs)
-    for pairsite = 1:pairsnum
+    for pairsite in 1:pairsnum
         site1 = 2 * pairsite - 1
         site2 = 2 * pairsite
         sites = [site1, site2]
         local_swap_op = create_local_swap_op(bitsnum, sites, siteindices)
-        op = apply(local_swap_op, op)
+        op = apply(local_swap_op, op; cutoff=1e-12)
     end
-    return op 
+    return op
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
     using RandomMeas
     # settings 
     N = 8
-    site_indices = siteinds("Qubit",N)
+    site_indices = siteinds("Qubit", N)
     # local swap op
     local_swap_op = create_local_swap_op(N, [1, N], site_indices)
     @show linkdims(local_swap_op)
     # reflect op
-    reflect_op = create_reflect_op(N, site_indices); 
+    reflect_op = create_reflect_op(N, site_indices)
     @show linkdims(reflect_op)
     # swap op but adjacent
-    adjacent_swap_op = create_adjacent_swap_op(N, site_indices); 
+    adjacent_swap_op = create_adjacent_swap_op(N, site_indices)
     @show linkdims(adjacent_swap_op)
 end
